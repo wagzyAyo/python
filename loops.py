72f1974ac345b4e1cce866763445d8491e287249
ayo <talktojmcvibes@gmail.com>
2023-09-09 07:35:17 -0700

    object oriented in python

--- loops.py
+++
@@ -1,7 +0,0 @@
-# use for loop through list of 1 to 21
-# use modulus to check that the result is NOT equal to 0
-# print the odds
-
-for i in range(1, 22):
-    if i % 2 != 0:
-        print("i = ", i)
--- trees.py
+++ trees.py
@@ -1,17 +1,6 @@
-tree_height = input("How big is your tree: ")
-tree_height = int(tree_height)
-spaces = tree_height - 1
-hashes = 1
-stump_spaces = tree_height - 1
-while tree_height != 0:
-    for i in range(spaces):
-        print(" ", end="")
-    for i in range(hashes):
-        print("#", end="")
-    print()
-    spaces -= 1
-    hashes += 2
-    tree_height -= 1
-for i in range(stump_spaces):
-    print(" ", end="")
-print("#")
+a
+id(a)
+139926795932424
+[1, 2, 3, 4]
+a = a + [5]
+id(a)
