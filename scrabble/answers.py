db67485d5c82820f7582b5958abc39c6b88ec1e4
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-08 04:48:04 -0700

    words

---
+++ scrabble/answers.py
@@ -0,0 +1,58 @@
+words = {
+    "iparmte": {
+        "matier",
+        "maitre",
+        "parietal",
+        "part",
+        "ramp",
+        "rate",
+        "trap",
+        "permit",
+        "pirate",
+        "armpit",
+        "imaret",
+        "armet",
+        "mater",
+        "prime",
+        "timer",
+        "tripe",
+        "ramie",
+        "retap",
+        "prate"
+    },
+    "tceubmir": {
+        "brucetim"
+    },
+    "tgrilhe": {
+        "lighter",
+        "reglith"
+    },
+    "tsboalc": {
+        "cobalts"
+    },
+    "ysgomnui": {
+        "gynoiums"
+    },
+    "renldaw": {
+        " wrangled",
+        "leward"
+    },
+    "ohtsimue": {
+        "outscheme",
+        "mouthies",
+        "eouthism"
+    },
+    "ceblayke": {
+        "lackey",
+        "blackeye"
+    },
+    "dnuwlera": {
+        "underlaw",
+        "launder"
+    },
+    "rgaine": {
+        "regain",
+        "gainer"
+    }
+
+}
