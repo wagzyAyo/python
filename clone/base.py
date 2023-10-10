998a636ec1024daaf121e1d5b27120a6331d5c75
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-10 07:33:35 -0700

    main base

--- clone/base.py
+++ clone/base.py
@@ -9,6 +9,17 @@ class BaseModel():
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_obj = self.__dict__.copy()
+        dict_obj["__class__"] = self.__class__.__name__
+        dict_obj["created_at"] = self.created_at.isoformat()
+        dict_obj["updated_at"] = self.updated_at.isoformat()
+
+        return dict_obj
+
     def __str__(self):
         class_name = self.__class__.__name__
-        return "{} {} {}".format(class_name, (self.id))
+        return "[{}] ({}) {}".format(class_name, self.id, self.to_dict)
