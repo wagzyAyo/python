3fbfd469d055e3fe52eb188f98c77670b1f1e81b
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-10 02:20:23 -0700

    prompt

--- command interpreter/command.py
+++
@@ -1,8 +0,0 @@
-import cmd
-
-
-class Prompt(cmd.Cmd):
-
-    def do_name(self):
-        name = input("What is your name? ")
-        print("Hello {} how are you doing today".format(name))
--- command_interpreter/command.py
+++ command_interpreter/command.py
@@ -3,11 +3,25 @@ import cmd
 
 class Prompt(cmd.Cmd):
 
+    NAMES = ['Alice', 'Adam', 'Barbara', 'Bob']
+
     def do_name(self, arg):
         """Set users name"""
         name = input("What is your name? ").title()
         print("Hello {} how are you doing today".format(name))
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """Return true and close the prompt"""
+        if arg == "^D":
+            return True
         return True
+
+    def complete_name(self, text, arg, begidx, endidx):
+        """ Auto complete names """
+
+        if not text:
+            comp = self.NAMES[:]
+        else:
+            comp = [f for f in self.NAMES if f.startswith(text)]
+
+        return comp
