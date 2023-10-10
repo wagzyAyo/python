ad365a0b1698142e573aab126b48befc2af1156a
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-10 12:33:57 -0700

    base

--- clone/base.py
+++ clone/base.py
@@ -1,19 +1,26 @@
+#!/usr/bin/python3
+"""This module contains BaseModel class"""
+
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel():
+    """The Base model class"""
 
     def __init__(self):
+        """initializing base model"""
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def save(self):
+        """Update public attribute with the cuttent date time"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        dict_obj = self.__dict__.copy()
+        """create a dict """
+        dict_obj = self.__dict__
         dict_obj["__class__"] = self.__class__.__name__
         dict_obj["created_at"] = self.created_at.isoformat()
         dict_obj["updated_at"] = self.updated_at.isoformat()
@@ -21,5 +28,6 @@ class BaseModel():
         return dict_obj
 
     def __str__(self):
+        """Magic method that define human readable of BaseModel object"""
         class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.to_dict)
+        return "[{}] ({}) {}".format(class_name, self.id, self.to_dict())
