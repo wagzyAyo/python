f3b9a98d594112252e0f7c68bc19c8544187df2a
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-15 11:42:49 -0700

    amenity.py

---
+++ clone/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains Amenity class that inherit from BaseModel"""
+from base import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity Class attributes """
+    name = ""
