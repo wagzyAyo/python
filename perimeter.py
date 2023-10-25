640363c32b7f59d3a5a05b65e89ccc3b8c501c58
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-25 11:48:21 -0700

    perimeter

---
+++ perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contains a function
+that calculates the perimeter of
+a given grid"""
+
+
+def island_perimeter(grid):
+    """This function calculates
+    perimeter of a grid"""
+    perimeter = 0
+    row = len(grid)
+    if grid != []:
+        column = len(grid[0])
+
+    for a in range(row):
+        for b in range(column):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == row or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeters += 1
+                if (b + 1) == column or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
