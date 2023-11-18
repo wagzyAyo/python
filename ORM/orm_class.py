cef271b7fc0f51b869c249cd548533b24fd99d88
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-18 02:13:03 -0800

    base

--- ORM/orm_class.py
+++ ORM/orm_class.py
@@ -3,10 +3,11 @@
 This module contains the class definition of a State
 and an instance Base = declarative_base()
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+my_meta = MetaData()
+Base = declarative_base(metadata=my_meta)
 
 
 class State(Base):
