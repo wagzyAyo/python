b09ee451d9f6be5349672a185864a86427aa03ea
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-18 03:31:44 -0800

    filter_a

--- ORM/filter_a.py
+++ ORM/filter_a.py
@@ -11,13 +11,13 @@ import sys
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.arg[1], sys.argv[2], sys.argv[3]),
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).order_by(State.id).filter('%a%')
+    states_with_a = session.query(State).filter(State.name.contains('a'))
 
     for state in states_with_a:
         print('{}: {}'.format(state.id, state.name))
