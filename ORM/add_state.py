e55631c8dbbb675938363fb88b6b08492ef95e8a
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-18 04:50:26 -0800

    add state

---
+++ ORM/add_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Add new state to the db
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
