8e7f806b751fc81e17460f287c6ca95082a9e3c4
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2024-01-06 20:39:24 +0100

    CORS

---
+++ blueprint/README.md
@@ -0,0 +1,1 @@
+# flask blueprint and CORS tutorial
--- blueprint/main.py
+++ blueprint/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from bp.hello import helloworld_bp
 from bp.calculator.calculator import calculator_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(helloworld_bp)
 app.register_blueprint(calculator_bp, url_prefix='/calculator')
 
