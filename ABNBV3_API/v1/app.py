33304c48f86fae8c6db366f5915fb46ec6fc9321
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2024-01-07 18:36:40 +0100

    module import error

--- ABNBV3_API/v1/app.py
+++ ABNBV3_API/v1/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from os import getenv
 from flask_cors import CORS
+from v1.views import app_views
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 host = getenv('HBNB_API_HOST')
 port = getenv('HBNB_API_PORT')
 cors = CORS(app=app)
+
+@app.route('/')
+def home():
+    return {'str': 'Hello world'}
 
 if __name__ == '__main__':
     if host is None:
