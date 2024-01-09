1cf03703c8121fc7eea9145090cef7e2cd874478
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2024-01-09 17:47:51 +0100

    linked list

---
+++ linked list/main.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def insertAtBegin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
