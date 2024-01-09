dc5850272bc517d86db4261d21788c998957f9e1
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2024-01-09 17:35:19 +0100

    loops

--- loops.py
+++ loops.py
@@ -1,8 +1,14 @@
 data = [2, 3, 4, 5, 6, 7, 8, 9]
+grades = ['A1','B2','B3','C4','C5','C6','D7','E8','F9']
+names = ['Ayo', 'Ola']
 total = 0
 
 for number in data:
     total += number
 
+for grade in grades:
+    for name in names:
+        print(f"{name} your grade is {grade}" )
 
-print(total)
+
+#print(total)
