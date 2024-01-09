8eac0e920cf88b3b302014e9fef5dab0a0041e7f
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2024-01-09 17:58:44 +0100

    insert at index

--- linked list/main.py
+++ linked list/main.py
@@ -11,3 +11,20 @@ class Node:
         else:
             new_node.next = self.head
             self.head = new_node
+
+    def insertAtIndex(self,data, index):
+        new_node = Node(data)
+        current_node = self.head
+        position = 0
+        if position == int(index):
+            self.insertAtBegin(data)
+        else:
+            while current_node != None and position+1 != int(index):
+                position+= 1
+                current_node = current_node.next
+
+                if current_node != None:
+                    new_node.next = current_node.next
+                    current_node.next = new_node
+                else:
+                    print('Index not present')
