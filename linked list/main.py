53503b16a1a789b36ed5525edb887bbad3708664
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2024-01-09 18:26:17 +0100

    updatenode

--- linked list/main.py
+++ linked list/main.py
@@ -45,3 +45,18 @@ class linkedlist:
             current_node = current_node.next
 
         current_node.next = new_node
+
+    def updateNode(self, val, index):
+        current_node = self.head
+        position = 0
+        if position == int(index):
+            current_node.data = val
+        else:
+            while (current_node != None and position != int(index)):
+                position+= 1
+                current_node = current_node.next
+
+                if current_node != None:
+                    current_node.data = val
+                else:
+                    print("Index not present")
